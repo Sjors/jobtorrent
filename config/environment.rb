ca6e60d060ffec49826abddaf5acb061603540c2
Sjors <sjors@sprovoost.nl>
2008-09-25 12:50:29 +1000

    Added paypal gem. Frozen gems.

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   
-  config.gem "sqlite3-ruby", :lib => "sqlite3", :version => '1.2.1'
+  config.gem "sqlite3-ruby", :lib => "sqlite3" #, :version => '1.2.1'
+
+  config.gem "paypal", :lib => "paypal"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
